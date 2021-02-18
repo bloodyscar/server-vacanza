59c8b9e39f9169ee1774c2d8a9ed4ade6b4d0e6e
bloodyscar <enggataukk@gmail.com>
2021-02-19 00:09:01 +0700

    install cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const methoderOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const cors = require('cors');
 
 // import mongoose 
 const mongoose = require('mongoose');
@@ -24,6 +25,7 @@ const apiRouter = require('./routes/api');
 
 var app = express();
 
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
